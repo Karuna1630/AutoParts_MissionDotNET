91aa7156509691c75c6e24bd4e5e0bc65aee18d0
Aases <aaseslimbu2@gmail.com>
2026-04-25 13:11:18 +0545

    common api response templete

---
+++ Backend/WebAPI/Common/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace WebAPI.Common
+{
+    /// <summary>
+    /// simple response object
+    /// </summary>
+    public class ApiResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Errors { get; set; }
+    }
+    /// <summary>
+    /// generic response object
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T? Data { get; set; }
+
+    }
+}
