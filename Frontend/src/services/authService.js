2886e21ccef96d602659855e929b9b9719278c7a
BishanTamang7 <realbishantamang@gmail.com>
2026-04-27 14:13:28 +0545

    Feat: Customer Login Complete

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -4,3 +4,8 @@ export const registerCustomer = async (payload) => {
   const response = await apiClient.post('/auth/register', payload);
   return response.data;
 };
+
+export const login = async (payload) => {
+  const response = await apiClient.post('/auth/login', payload);
+  return response.data;
+};
