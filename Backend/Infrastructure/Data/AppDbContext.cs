8c9de26d825d979a9339fe6af64b79932c48fde0
Karuna1630 <Karunagiri481@gmail.com>
2026-04-28 13:37:41 +0545

    Feat: Merged backend code with the staffregister api

--- Backend/Infrastructure/Data/AppDbContext.cs
+++ Backend/Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,8 @@
-﻿using Domain.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,34 +11,37 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
-    public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
-    {
-        public DbSet<UserProfile> UserProfiles { get; set; }
-   public DbSet<User> Users => Set<User>();
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public new DbSet<User> Users => Set<User>();
     public DbSet<Vehicle> Vehicles => Set<Vehicle>();
-        protected override void OnModelCreating(ModelBuilder builder)
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        builder.Entity<UserProfile>(entity =>
         {
-            base.OnModelCreating(builder);
-     modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
-            builder.Entity<UserProfile>(entity =>
-            {
-                entity.ToTable("UserProfiles"); 
-                entity.HasKey(x => x.IdentityId);    
+            entity.ToTable("UserProfiles"); 
+            entity.HasKey(x => x.IdentityId);    
 
-                entity.Property(x => x.FirstName)
-                    .IsRequired()
-                    .HasMaxLength(100);
+            entity.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
 
-                // Setup the Relationship to IdentityUser
-                entity.HasOne<ApplicationUser>()
-                    .WithOne()
-                    .HasForeignKey<UserProfile>(x => x.IdentityId)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-        }
+            // Setup the Relationship to IdentityUser
+            entity.HasOne<ApplicationUser>()
+                .WithOne()
+                .HasForeignKey<UserProfile>(x => x.IdentityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
 
-       
+        // Ensure Domain.Entities.User is mapped correctly if not handled by configurations
+        builder.Entity<User>(entity => {
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
     }
 }
